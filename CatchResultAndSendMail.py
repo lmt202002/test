35a750f2482f598bd0e606205b040b5c279e11ae
lvshr <lvshr@vip.qq.com>
2017-03-09 11:19:19 +0800

    更新代码

--- CatchResultAndSendMail.py
+++ CatchResultAndSendMail.py
@@ -1,8 +1,10 @@
 #-*-coding=utf-8 -*-
 from mylibs.ecm import ECMlibs
-
-resultdir='E:\\test\\testresult'
-newfile=ECMlibs(object)
-filesource=newfile.CatchNewFile(resultdir)
-newfile.SentMail(filesource)
-
+import os
+def SendResult():
+    currentdir=os.getcwd()
+    # print(currentdir)
+    resultdir=currentdir+'\\testresult'
+    newfile=ECMlibs(object)
+    filesource=newfile.CatchNewFile(resultdir)
+    newfile.SentMail(filesource)
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 #coding=utf8
-import nose
-
-result=nose.run()
-print('运行结果：%s'%result)
+import CatchResultAndSendMail
+CatchResultAndSendMail.SendResult()
